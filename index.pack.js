fdbdeab1c74e10056f1f3f932b0d6a1fc9d7d606
Lunkaz <marialbornozapata@gmail.com>
2021-10-11 23:14:28 -0300

    drawing ghosts

--- index.pack.js
+++ index.pack.js
@@ -2,6 +2,7 @@ const width = 28;
 const grid = document.querySelector(".grid");
 const scoreDisplay = document.querySelector("#score");
 let squares = [];
+let score = 0;
 
 const layout = [
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
@@ -44,6 +45,8 @@ function createBoard() {
             squares[i].classList.add('pac-dot');
         } else if (layout[i] === 1) {
             squares[i].classList.add('wall')
+        } else if (layout[i] === 2) {
+            squares[i].classList.add("ghost-lair")
         } else if (layout[i] === 3) {
             squares[i].classList.add("power-pellet")
         }
@@ -63,27 +66,87 @@ function control(event) {
 
         case "Down":
         case "ArrowDown":
-            if (pacmanCurrentIndex + width < width * width) pacmanCurrentIndex += width
+            if (!squares[pacmanCurrentIndex + width].classList.contains('ghost-lair') && 
+            !squares[pacmanCurrentIndex + width].classList.contains("wall") && 
+            pacmanCurrentIndex + width < width * width) pacmanCurrentIndex += width
             break;
 
         case "Up":
         case "ArrowUp":
-            if (pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -= width
+            if (!squares[pacmanCurrentIndex - width].classList.contains('ghost-lair') && 
+            !squares[pacmanCurrentIndex - width].classList.contains("wall") && 
+            pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -= width
             break;
 
         case "Left":
         case "ArrowLeft":
-            if (pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -= 1
+            if (!squares[pacmanCurrentIndex - 1].classList.contains('ghost-lair') && 
+            !squares[pacmanCurrentIndex - 1].classList.contains("wall") && 
+            pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -= 1
+            if (pacmanCurrentIndex === 364) {
+                pacmanCurrentIndex = 391
+            }
             break;
 
         case "Right":
         case "ArrowRight":
-            if (pacmanCurrentIndex % width < width - 1) pacmanCurrentIndex += 1
+            if (!squares[pacmanCurrentIndex + 1].classList.contains('ghost-lair') && 
+            !squares[pacmanCurrentIndex + 1].classList.contains("wall") && 
+            pacmanCurrentIndex % width < width - 1) pacmanCurrentIndex += 1
+            if (pacmanCurrentIndex === 391) {
+                pacmanCurrentIndex = 364
+            }
             break;
         
         default:
             return; 
     }
     squares[pacmanCurrentIndex].classList.add("pacman");
+    pacDotEaten();
 }
 document.addEventListener('keyup', control)
+
+function pacDotEaten() {
+    if (squares[pacmanCurrentIndex].classList.contains("pac-dot")) {
+        score++;
+        scoreDisplay.textContent = score;
+        squares[pacmanCurrentIndex].classList.remove("pac-dot");
+    }
+}
+
+class Ghost {
+    constructor(className, startIndex, speed) {
+        this.className = className
+        this.startIndex = startIndex
+        this.speed = speed
+    }
+}
+
+const ghosts = [
+    new Ghost("blinky", 348, 250),
+    new Ghost("pinky", 376, 400),
+    new Ghost("inky", 351, 300),
+    new Ghost("clyde", 379, 500)
+]
+
+ghosts.forEach(ghost => squares[ghost.startIndex].classList.add(ghost.className));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
